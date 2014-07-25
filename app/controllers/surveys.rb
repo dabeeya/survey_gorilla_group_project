cd91b0ab7a5e4a1935f1e666a6e11a9c6e680c9e
Dylan Krause <dykrause@gmail.com>
2014-07-25 17:24:55 -0400

    Edit Survey View Add

--- app/controllers/surveys.rb
+++ app/controllers/surveys.rb
@@ -1,9 +1,17 @@
-get '/surveys/:id' do
-end
-
 get '/surveys/new' do
   erb :create
 end
+
+
+get '/surveys/:id' do
+end
+
+
+#Maker Edit Page
+get '/surveys/:id/maker' do
+
+	erb :edit_survey 
+end 
 
 patch '/surveys/:id/edit' do 
 end
