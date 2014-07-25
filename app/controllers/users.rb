bde437923a861aacf985132f5ae142e7edfe2ac0
Derek <derek.siker@gmail.com>
2014-07-25 18:35:11 -0400

    changed routing for sessions in the user controller to match forms in index view

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -2,11 +2,41 @@ get '/users/:id' do
   erb :profile 
 end
 
-post '/users/login' do 
+
+#----------- SESSIONS -----------
+
+#SIGN-IN
+post '/users' do
+  # sign-in
+  @email = params[:email]
+  user = User.authenticate(@email, params[:password])
+  if user
+    # successfully authenticated; set up session and redirect
+    session[:user_id] = user.id
+    redirect '/users/#{user.id}'
+  else
+    # an error occurred, re-render the sign-in form, displaying an error
+    @error = "Invalid email or password."
+    erb :index
+  end
 end
 
-post '/users/sign_up' do 
+
+#SIGN-UP
+post '/users/new' do 
+  @user = User.create(params[:user])
+  if @user.save
+    session[:user_id] = @user.id
+    redirect '/users/#{user.id}'  
+  else
+    erb :index
 end
 
+
+#LOGOUT
 get '/users/logout' do 
+  session.clear
+  redirect '/'
 end
+
+
