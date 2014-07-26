012023ba0de2102caa9839b5d8adc1de1f0767fb
Dylan Krause <dykrause@gmail.com>
2014-07-26 18:27:33 -0400

    Setup Up Functional But Unstyled Survey

--- app/controllers/surveys.rb
+++ app/controllers/surveys.rb
@@ -2,6 +2,19 @@ get '/surveys/new' do
   erb :create
 end
 
+post '/surveys/submit' do
+	puts params
+	params.each do |question, input|
+		choices = Choice.where(question_id: question, input: input)
+		if choices.empty?
+			choices << Choice.create(input: input, question_id: question.to_s)
+		end
+		Response.create(choice_id: choices.first.id, user_id: current_user.id)
+	end
+	redirect "users/#{current_user.id}"
+
+	end
+
 get '/surveys/:id' do
 	@survey = Survey.find(params[:id])
 	@num = 0
