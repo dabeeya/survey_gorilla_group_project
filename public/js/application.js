0468c6bebaeeb5031e4653659bd0ee74ea5d5063
Farheen Malik <farheen.m.malik@gmail.com>
2014-07-26 23:01:21 -0400

    Made view of survey on create survey page more readable - still not that sleek

--- public/js/application.js
+++ public/js/application.js
@@ -1,5 +1,4 @@
 $(document).ready(function(){
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
   $('#question-options').hide();
   $('#radio-option').hide();
   $('#text-option').hide();
@@ -69,28 +68,3 @@ $(document).ready(function(){
     })
   })
 })
-
-
-
-// Thanks internet!
-
-//create three initial fields
-// var startingNo = 3;
-// var $node = "";
-// for(varCount=0;varCount<=startingNo;varCount++){
-//     var displayCount = varCount+1;
-//     $node += '<p><label for="var'+displayCount+'">Variable '+displayCount+': </label><input type="text" name="var'+displayCount+'" id="var'+displayCount+'"><span class="removeVar">Remove Variable</span></p>';
-// }
-// //add them to the DOM
-// $('form').prepend($node);
-// //remove a textfield
-// $('form').on('click', '.removeVar', function(){
-//    $(this).parent().remove();
-
-// });
-// //add a new node
-// $('#addQuestion').on('click', function(){
-// varCount++;
-// $node = '<p><label for="var'+varCount+'">Variable '+varCount+': </label><input type="text" name="var'+varCount+'" id="var'+varCount+'"><span class="removeVar">Remove Variable</span></p>';
-// $(this).parent().before($node);
-// });
